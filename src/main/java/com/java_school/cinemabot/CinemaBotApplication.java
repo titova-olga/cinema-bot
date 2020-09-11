ecd1edb01d5ceb24d227dc295c20f337230c9433
Olga <olya11aprel1999@mail.ru>
2020-09-11 18:16:12 +0300

    Initial commit

---
+++ src/main/java/com/java_school/cinemabot/CinemaBotApplication.java
@@ -0,0 +1,13 @@
+package com.java_school.cinemabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java_school/cinemabot/CinemaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_school.cinemabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
