f773af9998f5172d1361afd270f897e1c518f2cb
kseniyakuznetsova <kseniya9696@gmail.com>
2020-09-11 19:34:11 +0300

    Create telegram bot component

--- src/main/java/com/java_school/cinemabot/CinemaBotApplication.java
+++ src/main/java/com/java_school/cinemabot/CinemaBotApplication.java
@@ -1,13 +1,25 @@
 package com.java_school.cinemabot;
 
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CinemaBotApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CinemaBotApplication.class, args);
+    @Bean
+    public TelegramBotsApi telegramBotsApi(){
+        return new TelegramBotsApi();
     }
 
+    @SneakyThrows
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        SpringApplication.run(CinemaBotApplication.class, args);
+    }
 }
---
+++ src/main/java/com/java_school/cinemabot/TelegramBot.java
@@ -0,0 +1,59 @@
+package com.java_school.cinemabot;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String botUsername;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Autowired
+    private  TelegramBotsApi telegramBotsApi;
+
+    @SneakyThrows
+    @PostConstruct
+    public void registerBot(){
+        telegramBotsApi.registerBot(this);
+    }
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        String messageText = message.getText();
+        User from = message.getFrom();
+        String fullInfo = "From " + from.getFirstName() + " " + from.getLastName() + ": " + messageText;
+        System.out.println(fullInfo);
+        sendMsg(message.getChatId().toString(), fullInfo);
+    }
+
+    @SneakyThrows
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        execute(sendMessage);
+    }
+
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    public String getBotToken() {
+        return botToken;
+    }
+}
