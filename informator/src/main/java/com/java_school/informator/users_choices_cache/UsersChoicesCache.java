31d50837a6c5c2a11931761424231f0776de5d95
Olga <titovaolga1104@gmail.com>
2020-09-20 15:08:39 +0300

    Added kafka

--- informator/src/main/java/com/java_school/informator/users_choices_cache/UsersChoicesCache.java
+++ informator/src/main/java/com/java_school/informator/users_choices_cache/UsersChoicesCache.java
@@ -1,18 +1,13 @@
 package com.java_school.informator.users_choices_cache;
 
 
-import com.java_school.informator.dto.CinemaUserChoiceDTO;
-import com.java_school.informator.dto.ClearUserChoiceDTO;
-import com.java_school.informator.dto.DateUserChoiceDTO;
-import com.java_school.informator.dto.FilmUserChoiceDTO;
 import com.java_school.informator.kafka.KafkaConsumer;
-import kafka.consumer.Consumer;
-import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerTimeoutException;
 import kafka.consumer.KafkaStream;
-import kafka.javaapi.consumer.ConsumerConnector;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -54,68 +49,9 @@ public class UsersChoicesCache {
         usersChoicesMap.remove(chatId);
     }
 
-    //@EventListener(ContextRefreshedEvent.class)
     @PostConstruct
     public void fillCacheFromKafkaOnRestart() {
-        getData();
-
-    }
-
-    private void getData() {
-        Map<String, Integer> topicMap = new HashMap<>();
-        // Define single thread for topic
-        topicMap.put(kafkaConsumer.getTopic(), 1);
-        try {
-            Map<String, List<KafkaStream<byte[], byte[]>>> listMap =
-                    kafkaConsumer.getConsumerConnector().createMessageStreams(topicMap);
-            List<KafkaStream<byte[], byte[]>> kafkaStreams = listMap.get(kafkaConsumer.getTopic());
-
-            // Collect the messages.
-            kafkaStreams.forEach(ks -> ks.forEach(mam -> parseUserChoice(new String(mam.message()))));
-
-        } catch (ConsumerTimeoutException exception) {
-            System.out.println("Filling cache ends");
-        } finally {
-            if (kafkaConsumer.getConsumerConnector() != null) {
-                //kafkaConsumer.getConsumerConnector().shutdown();
-            }
-        }
-    }
-
-    @SneakyThrows
-    private void parseUserChoice(String message) {
-        System.out.println(message);
-        int startParamsInd = message.indexOf("{");
-        int endParamsInd = message.indexOf("}");
-        if(startParamsInd == -1|| endParamsInd == -1)
-            return;
-
-        String className = message.substring(0, startParamsInd);
-
-        //Object obj = Class.forName(className).getDeclaredConstructor().newInstance();
-
-        if (className.equals("ClearUserChoiceDTO")) {
-            int chatId = Integer.parseInt(message
-                    .substring(startParamsInd + 1, endParamsInd)
-                    .split("=")[1]);
-            removeInfoAboutUserChoices(chatId);
-        } else {
-            String[] parameters = message.substring(startParamsInd + 1, endParamsInd)
-                    .split(",");
-            int chatId = Integer.parseInt(parameters[0].split("=")[1]);
-
-            if (className.equals("FilmUserChoiceDTO")) {
-                int choiceId = Integer.parseInt(parameters[1].split("=")[1]);
-                addFilmChoice(chatId, choiceId);
-            }
-            if (className.equals("CinemaUserChoiceDTO")) {
-                int choiceId = Integer.parseInt(parameters[1].split("=")[1]);
-                addCinemaChoice(chatId, choiceId);
-            }
-            if (className.equals("DateUserChoiceDTO")) {
-                LocalDate date = LocalDate.parse(parameters[1].split("=")[1]);
-                addDateChoice(chatId, date);
-            }
-        }
+        kafkaConsumer.fillCache();
+        System.out.println();
     }
 }
